1c4138064a16698c754c22fe0133f4a53f65741a
Syed Quadri <syed.quadri@hashicorp.com>
2024-09-13 17:07:28 -0500

    updates

--- func.py
+++ func.py
@@ -1,22 +1,27 @@
-import datetime
 import json
-import os
 import subprocess
 import time
-import config
 import pandas as pd
 import requests
+import config
 import utilities
-from datetime import datetime, timedelta
 
 
 def get_request(q):
+    """
+    Sends a POST request to the API endpoint defined in the config.
+    The request contains a query, and the response is returned as JSON.
+    """
     return requests.post(config.ENDPOINT, headers={"Authorization": "Bearer " + config.ACCESS_TOKEN},
                          json={"query": q}).json()
 
 
-def get_track_plays_by_month(track_slugs, org_slug=config.ORG_SLUG, filter_developers=config.FILTER_DEVELOPERS,
-                             dates=utilities.month_map(config.YEAR)):
+def get_track_plays_by_month(track_slugs):
+    """
+    Queries the API for the track plays by month for each track slug.
+    Returns a table of results with track titles and their corresponding plays per month.
+    """
+    dates = utilities.month_map(config.YEAR)
     slug_data = []
     for track_slug in track_slugs:
         print(track_slug)
@@ -24,7 +29,7 @@ def get_track_plays_by_month(track_slugs, org_slug=config.ORG_SLUG, filter_devel
         for k, v in dates.items():
             query = f"""
                 query {{
-                    statistics(trackSlug: "{track_slug}", organizationSlug: "{org_slug}", filterDevelopers: {filter_developers}, start: "{k}", end: "{v}") {{
+                    statistics(trackSlug: "{track_slug}", organizationSlug: "{config.ORG_SLUG}", filterDevelopers: {config.FILTER_DEVELOPERS}, start: "{k}", end: "{v}") {{
                         track {{
                             title
                             started_total
@@ -44,7 +49,31 @@ def get_track_plays_by_month(track_slugs, org_slug=config.ORG_SLUG, filter_devel
         slug_data.append(row)
     return create_table(slug_data)
 
+
 def get_track_slugs():
+    """
+        Queries the API for all track slugs associated with the organization.
+        Returns a list of track slugs.
+
+        Usage:
+        1. Ensure that the 'config.py' file contains the correct organization slug (ORG_SLUG).
+        2. This function makes a request to the API and retrieves all available track slugs for the organization.
+        3. The function returns a list of slugs, which can be used for other queries like fetching track statistics.
+
+        Example:
+            track_slugs = get_track_slugs()
+
+        Output:
+        A list of strings where each string is a track slug, for example:
+        ['track1', 'track2', 'track3']
+
+        Dependencies:
+        - Requires a properly configured 'config.py' file with `ORG_SLUG`.
+        - Requires the `get_request` function to handle API communication.
+
+        Note:
+        - Ensure you have network access and a valid access token to query the API.
+        """
     query = f"""query {{
            tracks(organizationSlug: "{config.ORG_SLUG}") {{
              slug
@@ -52,12 +81,14 @@ def get_track_slugs():
        }}"""
 
     output = get_request(query)
-    print(output)
-    slugs = [track['slug'] for track in output.json()['data']['tracks']]
+    slugs = [track['slug'] for track in output['data']['tracks']]
     return slugs
 
 
-def create_table(data, file_type='csv', file_name='tracks_by_month'):
+def create_table(data):
+    """
+    Creates a table (DataFrame) from the provided data and exports it to a file.
+    """
     # Define the months as the column headers
     months = ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan']
 
@@ -68,37 +99,65 @@ def create_table(data, file_type='csv', file_name='tracks_by_month'):
     # Create the DataFrame with topics as the index and months as the columns
     df = pd.DataFrame(values, index=topics, columns=months)
 
-    file_path = f'outputs/{file_name}.csv'
+    file_path = f'tracks_by_month.csv'
     df.to_csv(file_path)
 
     print(f"Data has been saved to {file_path}")
 
 
-def get_tracks_scores(file_name='track_scores'):
-    query = f"""query {{
-               tracks(organizationSlug: "{config.ORG_SLUG}") {{
-                 slug
-                 statistics {{
-                    title
-                    started_total
-                    completed_total
-                    average_review_score
-                    }}
-               }}
-           }}"""
+def get_tracks_scores(track_slugs):
+    """
+    Queries the API for each track's statistics such as title, total started, total completed,
+    and average review score. It computes the percentage of completions and saves the data
+    to a CSV file.
 
-    data = get_request(query)
+    Usage:
+    1. Provide a list of track slugs as input. For example:
+        track_slugs = ['track1', 'track2', 'track3']
+    2. Ensure that the 'config.py' file contains proper configurations, such as ORG_SLUG.
+    3. The function will create an output file 'track_scores.csv' in the 'outputs' directory,
+       containing the statistics for each track.
 
+    Example:
+        track_slugs = ['slug1', 'slug2']
+        get_tracks_scores(track_slugs)
+
+    Output:
+    A CSV file named 'track_scores.csv' with the following columns:
+    - Title: The title of the track.
+    - Started: The total number of users who started the track.
+    - Completed: The total number of users who completed the track.
+    - Percent Completed: The percentage of users who completed the track.
+    - Average Review Score: The average review score for the track.
+    """
     # Initialize an empty list to hold the data for each track
     d = []
 
-    # Iterate over the tracks in the JSON data
-    for track in data['data']['tracks']:
-        # Extract relevant statistics
-        title = track['statistics']['title']
-        started = track['statistics']['started_total']
-        completed = track['statistics']['completed_total']
-        average_review_score = track['statistics']['average_review_score']
+    # Iterate over each track slug and make a query for each
+    for track_slug in track_slugs:
+        # Define the query for the specific track_slug
+        query = f"""
+            query {{
+                statistics(trackSlug: "{track_slug}", organizationSlug: "{config.ORG_SLUG}") {{
+                    track {{
+                        title
+                        started_total
+                        completed_total
+                        average_review_score
+                    }}
+                }}
+            }}
+        """
+
+        # Get data for this specific track
+        data = get_request(query)
+
+        # Extract relevant statistics from the response
+        track_data = data['data']['statistics']['track']
+        title = track_data['title']
+        started = track_data['started_total']
+        completed = track_data['completed_total']
+        average_review_score = track_data['average_review_score']
 
         # Calculate percent completed and round to 2 decimal places
         percent_completed = round((completed / started * 100), 2) if started > 0 else 0.00
@@ -120,10 +179,16 @@ def get_tracks_scores(file_name='track_scores'):
     df = pd.DataFrame(d)
 
     # Write the DataFrame to a CSV file
-    file_path = f'outputs/{file_name}.csv'
+    file_path = f'track_scores.csv'
     df.to_csv(file_path, index=False)
 
+
 def get_slugs_with_tag(tag_val):
+    """
+    Queries the API for tracks based on a specific tag value.
+    Writes the track titles associated with that tag to a text file.
+    Returns the track slugs that match the tag.
+    """
     query = f"""query {{
                tracks(organizationSlug: "{config.ORG_SLUG}") {{
                 slug
@@ -150,6 +215,9 @@ def get_slugs_with_tag(tag_val):
 
 
 def write_list_to_file(lines, name):
+    """
+    Writes the provided list of lines to a file under the 'outputs' directory.
+    """
     name = f"outputs/{name}_tag_output.txt"
     with open(name, 'w') as f:
         for line in lines:
@@ -157,6 +225,10 @@ def write_list_to_file(lines, name):
 
 
 def is_token_valid():
+    """
+    Checks if the stored token in the credentials file is still valid.
+    Returns True if valid, False otherwise.
+    """
     try:
         # Read the credentials file
         with open(config.credentials_file, 'r') as f:
@@ -184,6 +256,9 @@ def is_token_valid():
 
 
 def renew_token():
+    """
+    Renews the API token by running the 'instruqt auth login' command.
+    """
     try:
         # Run the instruqt auth login command to renew the token
         subprocess.run("instruqt auth login", shell=True, check=True)
@@ -193,100 +268,11 @@ def renew_token():
 
 
 def check_and_renew_token():
+    """
+    Checks if the token is valid; if not, renews the token.
+    """
     if is_token_valid():
         print("Token is still valid.")
     else:
         print("Token has expired or is invalid. Renewing token...")
         renew_token()
-
-
-def send_slack_message():
-    s = "This is an automated message to inform you that your Instruqt Hot start is not within guidelines. See the violations below: "
-
-    return 0
-
-
-def check_status(data):
-    """Check if status is anything except 'Deleted' or 'Expired'."""
-    status = data.get('status', '').lower()
-    return status not in ['deleted', 'expired']
-
-
-def check_dates(data):
-    """Check if ends_at is None or if the difference between starts_at and ends_at is greater than 24 hours."""
-    starts_at = data.get('starts_at')
-    ends_at = data.get('ends_at')
-
-    if not starts_at:
-        return False, "starts_at is missing."
-
-    starts_at_dt = datetime.fromisoformat(starts_at.replace('Z', '+00:00'))
-
-    if ends_at is None:
-        return False, "End time is not set"
-
-    ends_at_dt = datetime.fromisoformat(ends_at.replace('Z', '+00:00'))
-    if abs((ends_at_dt - starts_at_dt).total_seconds()) > 24 * 3600:
-        return False, "Pool TTL is greater than 24 hours"
-
-    return True, None
-
-
-def check_auto_refill(data):
-    """Check if auto_refill is True."""
-    auto_refill = data.get('auto_refill', False)
-    if auto_refill:
-        return False, "Auto fill is enabled"
-    return True, None
-
-
-def compile_message(data):
-    """Compile messages based on date checks and auto_refill checks, excluding the status."""
-    messages = []
-
-    for node in data['data']['hotStartPools']['nodes']:
-        node_messages = []
-        node_name = node['name']
-
-        # Check if the status requires further validation
-        if check_status(node):
-            # Check date validity
-            dates_check, dates_message = check_dates(node)
-            if not dates_check:
-                node_messages.append(dates_message)
-
-            # Check auto_refill
-            auto_refill_check, auto_refill_message = check_auto_refill(node)
-            if not auto_refill_check:
-                node_messages.append(auto_refill_message)
-
-        # Format the violations into a cohesive message
-        if node_messages:
-            email = node.get('created_by', {}).get('profile', {}).get('email', 'No email provided')
-            message = f"This is an automated message to inform you that your Instruqt Hot start '{node_name}' is not within guidelines. See the violations below:\n\n"
-            message += "\n".join(f"- {msg}" for msg in node_messages)
-            message += f"\nCreated by: {email}\n" + "-" * 40
-            messages.append(message)
-
-    return "\n\n".join(messages)
-
-
-def get_hotpool_violations():
-    query = f"""query {{
-                   hotStartPools(organizationSlug: "{config.ORG_SLUG}") {{
-                    nodes {{
-                        name
-                        starts_at
-                        ends_at
-                        status
-                        auto_refill
-                        created_by {{
-                            profile {{
-                            email
-                            }}
-                        }}
-                        }}
-                   }}
-               }}"""
-
-    print(compile_message(get_request(query)))
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-import json
-
-import config
 import func
-from func import get_request, compile_message, get_hotpool_violations, get_track_slugs, get_tracks_scores
 
-
+# Usage Instructions:
+# This script is designed to interact with an API, retrieve data for various tracks, and process the data.
+# Ensure the following:
+# 1. The `config.py` file must contain proper configurations, such as ENDPOINT, ACCESS_TOKEN, ORG_SLUG, etc.
+# 2. `utilities.py` should have a `month_map` function that maps months to start and end dates.
+# 3. Ensure valid credentials and use the token renewal function to maintain access.
 
 def main():
-    # func.check_and_renew_token()
-
-    get_hotpool_violations()
+    func.check_and_renew_token()
 
     tracks_tagged = func.get_slugs_with_tag("terraform")
     #
     # tracks_all = func.get_track_slugs()
     #
     # func.get_track_plays_by_month(tracks_tagged)
+
+    # func.get_tracks_scores("scores")
 
     hvd_tracks = ["health-assessments-and-run-tasks",
                   "terraform-modules-testing-and-lifecycle",
@@ -50,25 +51,13 @@ def main():
                   "enterprise-cluster-configuration",
                   "enterprise-cluster-dns-configuration"]
 
-    # query = f"""query {{
-    #            tracks(organizationSlug: "{config.ORG_SLUG}") {{
-    #              slug
-    #              statistics {{
-    #                 title
-    #                 started_total
-    #                 completed_total
-    #                 average_review_score
-    #                 }}
-    #            }}
-    #        }}"""
-    #
-    # print(get_request(query))
-    #
-    #
-    # get_tracks_scores("scores")
+    track_slugs = ["health-assessments-and-run-tasks",
+                  "terraform-modules-testing-and-lifecycle"]
+
+    func.get_tracks_scores(func.get_track_slugs())
 
 
-
+    print("Everything is done")
 
 if __name__ == "__main__":
     main()
